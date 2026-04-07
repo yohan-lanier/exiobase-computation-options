51faba54d21791e991c193761d2fe88b9b5de83b
yohan-lanier <yohan.lanier@gmail.com>
2026-04-07 13:45:35 +0200

    fix: add reference year to cached data file path
    + mini refacto

--- exon/exiobase/extract.py
+++ exon/exiobase/extract.py
@@ -1,6 +1,6 @@
 import logging
 from pathlib import Path
-from typing import cast
+from typing import TypedDict, cast
 
 import pandas as pd
 from packaging.version import Version
@@ -14,44 +14,56 @@ from exon.utils import ExiobaseRelevantData
 def extract_exiobase_data(version: str, reference_year: str) -> ExiobaseRelevantData:
     logging.info("Parsing exiobase version %s", version)
     folder_path = DATABASES / "exiobase" / f"{version}"
-    s_file_path = folder_path / "S.csv"
-    a_file_path = folder_path / "A.csv"
-    units_file_path = folder_path / "units.csv"
+    s_file_path = folder_path / f"S_{reference_year}.csv"
+    a_file_path = folder_path / f"A_{reference_year}.csv"
+    units_file_path = folder_path / f"units_{reference_year}.csv"
     if s_file_path.exists() and a_file_path.exists() and units_file_path.exists():
-        logging.info(
-            "Good news, found existing a, s and units matrices. Skipping pymrio extract"
-        )
-        return {
-            "a": pd.read_csv(
-                a_file_path, sep=",", low_memory=False, header=[0, 1], index_col=[0, 1]
-            ),
-            "s": pd.read_csv(
-                s_file_path, sep=",", low_memory=False, header=[0, 1], index_col=[0]
-            ),
-            "units": pd.read_csv(
-                units_file_path, sep=",", low_memory=False, index_col=[0]
-            ),
-            "reference_year": reference_year,
-        }
-    else:
-        if Version(version) >= Version("3.0"):  # TO DO, manage other years of exiobase
-            exiobase = parse_exiobase3(folder_path / f"IOT_{reference_year}_pxp.zip")
-        else:
-            logging.error(
-                "Parsing of version %s is not yet supported here, only >= v3 for now",
-                version,
-            )
-            raise NotImplementedError
-        exiobase_light_data = get_relevant_exiobase_data(
-            exiobase, version, reference_year
-        )
-        # Exiobase object is heavy, drop it once useless
-        del exiobase
-        cache_useful_data(
-            exiobase_light_data,
+        return open_cached_data(
             {"a": a_file_path, "s": s_file_path, "units": units_file_path},
+            reference_year,
         )
-        return exiobase_light_data
+    if Version(version) >= Version("3.0"):  # TO DO, manage other years of exiobase
+        exiobase = parse_exiobase3(folder_path / f"IOT_{reference_year}_pxp.zip")
+    else:
+        logging.error(
+            "Parsing of version %s is not yet supported here, only >= v3 for now",
+            version,
+        )
+        raise NotImplementedError
+    exiobase_light_data = get_relevant_exiobase_data(exiobase, version, reference_year)
+    # Exiobase object is heavy, drop it once useless
+    del exiobase
+    cache_useful_data(
+        exiobase_light_data,
+        {"a": a_file_path, "s": s_file_path, "units": units_file_path},
+    )
+    return exiobase_light_data
+
+
+class CachedDataPaths(TypedDict):
+    a: Path
+    s: Path
+    units: Path
+
+
+def open_cached_data(
+    file_paths: CachedDataPaths, reference_year: str
+) -> ExiobaseRelevantData:
+    logging.info(
+        "Good news, found existing a, s and units matrices. Skipping pymrio extract"
+    )
+    return {
+        "a": pd.read_csv(
+            file_paths["a"], sep=",", low_memory=False, header=[0, 1], index_col=[0, 1]
+        ),
+        "s": pd.read_csv(
+            file_paths["s"], sep=",", low_memory=False, header=[0, 1], index_col=[0]
+        ),
+        "units": pd.read_csv(
+            file_paths["units"], sep=",", low_memory=False, index_col=[0]
+        ),
+        "reference_year": reference_year,
+    }
 
 
 def get_relevant_exiobase_data(
@@ -92,15 +104,15 @@ def get_relevant_exiobase_data(
             reference_year,
         )
     else:
-        # Stressors are provided in unit/M.EUR -> for all stressors for which ref unit
-        # is not M.EUR we convert them to unit/euro.
-        # Stressors for which ref unit is M.EUR are in M.EUR/M.EUR so we don't convert them
         s_matrix = (
             parsed_exiobase.satellite.S.copy()  # pyright: ignore [reportAttributeAccessIssue]
         )
         units = (
             parsed_exiobase.satellite.unit.copy()  # pyright: ignore [reportAttributeAccessIssue]
         )
+    # Stressors are provided in unit/M.EUR -> for all stressors for which ref unit
+    # is not M.EUR we convert them to unit/euro.
+    # Stressors for which ref unit is M.EUR are in M.EUR/M.EUR so we don't convert them
     mask = units["unit"] != EXIOBASE_MEURO
     s_matrix.loc[mask] /= 1e6
     ## but we do need to update their reference unit to euros
@@ -117,7 +129,7 @@ def get_relevant_exiobase_data(
 
 
 def cache_useful_data(
-    exiobase_light_data: ExiobaseRelevantData, file_paths: dict[str, Path]
+    exiobase_light_data: ExiobaseRelevantData, file_paths: CachedDataPaths
 ) -> None:
     exiobase_light_data["s"].to_csv(file_paths["s"], sep=",", na_rep="#NA", index=True)
     exiobase_light_data["a"].to_csv(file_paths["a"], sep=",", na_rep="#NA", index=True)
