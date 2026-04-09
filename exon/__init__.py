432455ef06f72b53cc5da954b2f392420c3cb00c
yohan-lanier <yohan.lanier@gmail.com>
2026-04-09 17:51:05 +0200

    feat: make methods accessible

--- exon/__init__.py
+++ exon/__init__.py
@@ -1,3 +1,19 @@
+from exon.exiobase import build_exiobase_in_bw, extract_exiobase_data
+from exon.lcia_computations import run_bw_computations, run_direct_matrix_computation
+from exon.lcia_methods import LCIA_METHODS
 from exon.paths import DATA
+from exon.utils import (
+    extrat_cfs_for_method_and_drop_null_cfs,
+    generate_random_samples_for_computations,
+)
 
-__all__ = ["DATA"]
+__all__ = [
+    "build_exiobase_in_bw",
+    "DATA",
+    "extrat_cfs_for_method_and_drop_null_cfs",
+    "extract_exiobase_data",
+    "generate_random_samples_for_computations",
+    "LCIA_METHODS",
+    "run_bw_computations",
+    "run_direct_matrix_computation",
+]
